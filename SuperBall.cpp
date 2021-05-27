02a7025c52c62e861b6d86996b63991d9a9c548e
Rimma-iv33 <82371850+Rimma-iv33@users.noreply.github.com>
2021-05-27 20:09:43 +0300

    Управляемый шар
    Управляемый шар

---
+++ SuperBall.cpp
@@ -0,0 +1,69 @@
+#include "TXLib.h"
+
+void MoveBall ();
+
+int main()
+    {
+    txCreateWindow (1500, 800);
+
+    MoveBall();
+
+    return 0;
+    }
+
+void MoveBall()
+    {
+    int x  = 100, y  = 100,
+        vx = 5,   vy = 2,
+        ax = 0,   ay = 1;
+
+    int dt = 1;
+
+    txSetColor (TX_LIGHTCYAN, 3);
+    txSetFillColor (RGB(255, 255, 0));
+
+    while (!txGetAsyncKeyState (VK_ESCAPE))
+        {
+        txSetColor (RGB (x, y, 128), 3);
+        txSetFillColor (RGB (x/2, y/2, 128));
+        txCircle (x, y, 30);
+
+        vx += ax * dt;
+        vy += ay * dt;
+
+        x += vx * dt;
+        y += vy * dt;
+
+        if (x > 1500)
+            {
+            vx = -vx;
+            x = 1500;
+            }
+
+        if (y > 800)
+            {
+            vy = -vy;
+            y = 800;
+            }
+
+        if (x < 0)
+            {
+            vx = -vx;
+            x = 0;
+            }
+
+        if (y < 0)
+            {
+            vy = -vy;
+            y = 0;
+            }
+
+        if (txGetAsyncKeyState (VK_RIGHT)) vx++;
+        if (txGetAsyncKeyState (VK_LEFT))  vx--;
+        if (txGetAsyncKeyState (VK_UP))    vy++;
+        if (txGetAsyncKeyState (VK_DOWN))  vy--;
+        if (txGetAsyncKeyState (VK_SPACE)) vx = vy = 0;
+        txSleep (10);
+        }
+    }
+
