f51ae2f92aba91a17d7e84f4aaa11de14689771e
Rimma-iv33 <82371850+Rimma-iv33@users.noreply.github.com>
2021-06-16 22:30:41 +0300

    Структуры
    Структуры

--- ThreeBalls_Super.cpp
+++ ThreeBalls_Super.cpp
@@ -2,9 +2,28 @@
 
 void MoveBall ();
 
-void DrawBall (int x, int y, int vx, int vy, COLORREF color, COLORREF fillcolor);
-void ControlBall (int* vx, int* vy);
-void PhysicsBall (int* x, int* y, int* vx, int* vy, int ax, int ay, int dt);
+struct Ball
+    {
+    int x,  y,
+        vx, vy;
+    int r;
+
+    COLORREF color;
+    COLORREF fillcolor;
+    };
+
+void DrawBall    (struct Ball  ball);
+void PhysicsBall (struct Ball* ball, int ax, int ay, int dt);
+void ControlBall (struct Ball* ball1);
+void BoomBalls (struct Ball* ball1, struct Ball* ball2);
+void DrawScore (int score1, int score2);
+
+bool LeftGoal  (struct Ball ball);
+bool RightGoal (struct Ball ball);
+
+double Distance (struct Ball ball1, struct Ball ball2);
+
+//-------------------------------------------------------------
 
 int main()
     {
@@ -15,87 +34,169 @@ int main()
     return 0;
     }
 
+//-------------------------------------------------------------
+
 void MoveBall()
     {
-    int x1  = 100, y1  = 100,
-        vx1 = 3,   vy1 = 2,
-        ax1 = 0,   ay1 = 1;
+    HDC fon = txLoadImage("fonsuper.bmp");
 
-    int x2  = 500, y2  = 100,
-        vx2 = 5,   vy2 = 2,
-        ax2 = 0,   ay2 = 1;
+    Ball ball1 = {100, 100, 3, 2, 17, TX_LIGHTRED, TX_BLUE};
+    Ball ball2 = {500, 100, 5, 2, 27, TX_LIGHTRED, TX_PINK};
+    Ball ball3 = {300, 100, 5, 2, 21, TX_LIGHTRED, TX_YELLOW};
 
-    int x3  = 300, y3  = 300,
-        vx3 = 5,   vy3 = 2,
-        ax3 = 0,   ay3 = 1;
+    int ax = 0, ay = 1;
 
     int dt = 1;
 
+    int score1 = 0;
+    int score2 = 0;
+
     while (!txGetAsyncKeyState (VK_ESCAPE))
         {
-        DrawBall (x1, y1, vx1, vy1, TX_LIGHTRED, TX_BLUE);
-        DrawBall (x2, y2, vx2, vy2, TX_LIGHTRED, TX_PINK);
-        DrawBall (x3, y3, vx3, vy3, TX_LIGHTRED, TX_YELLOW);
+        txBegin ();
 
-        PhysicsBall (&x1, &y1, &vx1, &vy1, ax1, ay1, dt);
-        PhysicsBall (&x2, &y2, &vx2, &vy2, ax2, ay2, dt);
-        PhysicsBall (&x3, &y3, &vx3, &vy3, ax3, ay3, dt);
+        txBitBlt  (txDC(), 0, 0, 1500, 800, fon, 0, 0);
 
-        ControlBall (&vx1, &vy1);
+        DrawScore (score1, score2);
+
+        DrawBall (ball1);
+        DrawBall (ball2);
+        DrawBall (ball3);
+
+        PhysicsBall (&ball1, ax, ay, dt);
+        PhysicsBall (&ball2, ax, ay, dt);
+        PhysicsBall (&ball3, ax, ay, dt);
+
+        double dist = Distance (ball1, ball2);
+
+        if (dist <= ball1.r + ball2.r)
+            {
+            BoomBalls (&ball1, &ball2);
+            }
+
+        if (LeftGoal  (ball1)) score1++;
+        if (RightGoal (ball1)) score2++;
+
+        ControlBall (&ball1);
+
+        txEnd ();
 
         txSleep (10);
         }
+    txDeleteDC (fon);
     }
 
-void DrawBall (int x, int y, int vx, int vy, COLORREF color, COLORREF fillcolor)
+//-------------------------------------------------------------
+
+void BoomBalls (struct Ball* ball1, struct Ball* ball2)
     {
-    txSetColor (color, 3);
-    txSetFillColor (fillcolor);
-
-    txCircle (x, y, 30);
-    txLine   (x, y, x + vx*5, y + vy*5);
-    txCircle (x + vx*5, y + vy*5, 3);
+    (*ball1).vx = -(*ball1).vx;
+    (*ball1).vy = -(*ball1).vy;
+    (*ball2).vx = -(*ball2).vx;
+    (*ball2).vy = -(*ball2).vy;
     }
 
-void ControlBall (int* vx, int* vy)
+//-------------------------------------------------------------
+
+double Distance (struct Ball ball1, struct Ball ball2)
     {
-    if (txGetAsyncKeyState (VK_RIGHT)) (*vx)++;
-    if (txGetAsyncKeyState (VK_LEFT))  (*vx)--;
-    if (txGetAsyncKeyState (VK_UP))    (*vy)++;
-    if (txGetAsyncKeyState (VK_DOWN))  (*vy)--;
-
-    if (txGetAsyncKeyState (VK_SPACE)) *vx = *vy = 0;
+    return sqrt ((ball1.x - ball2.x)*(ball1.x - ball2.x) + (ball1.y - ball2.y)*(ball1.y - ball2.y));
     }
 
-void PhysicsBall (int* x, int* y, int* vx, int* vy, int ax, int ay, int dt)
+//-------------------------------------------------------------
+
+void DrawBall (struct Ball ball)
     {
-    *vx = *vx + ax * dt;
-    *vy = *vy + ay * dt;
+    txBegin ();
+    txSetColor (ball.color, 2);
+    txSetFillColor (ball.fillcolor);
 
-    *x  = *x + *vx * dt;
-    *y  = *y + *vy * dt;
+    txCircle (ball.x, ball.y, ball.r);
+    txLine   (ball.x, ball.y, ball.x + ball.vx*5, ball.y + ball.vy*5);
+    txCircle (ball.x + ball.vx*5, ball.y + ball.vy*5, 3);
+    txEnd ();
+    }
 
-    if (*x > 1500)
+//-------------------------------------------------------------
+
+void ControlBall (struct Ball* ball1)
+    {
+    if (txGetAsyncKeyState (VK_RIGHT)) (*ball1).vx = (*ball1).vx + 1;
+    if (txGetAsyncKeyState (VK_LEFT))  (*ball1).vx = (*ball1).vx - 1;
+    if (txGetAsyncKeyState (VK_UP))    (*ball1).vy = (*ball1).vy - 1;
+    if (txGetAsyncKeyState (VK_DOWN))  (*ball1).vy = (*ball1).vy + 1;
+
+    if (txGetAsyncKeyState (VK_SPACE)) (*ball1).vx = (*ball1).vy = 0;
+    }
+
+//-------------------------------------------------------------
+
+void PhysicsBall (struct Ball* ball, int ax, int ay, int dt)
+    {
+    (*ball).vx = (*ball).vx + ax * dt;
+    (*ball).vy = (*ball).vy + ay * dt;
+
+    (*ball).x  = (*ball).x + (*ball).vx * dt;
+    (*ball).y  = (*ball).y + (*ball).vy * dt;
+
+    if ((*ball).x > 1500 - (*ball).r)
         {
-        *vx = - *vx;
-        *x = 1500;
+        (*ball).vx = -(*ball).vx;
+        (*ball).x  = 1500 - (*ball).r;
         }
 
-    if (*y > 800)
+    if ((*ball).y > 800 - (*ball).r)
         {
-        *vy = - *vy;
-        *y = 800;
+        (*ball).vy = -(*ball).vy;
+        (*ball).y  = 800 - (*ball).r;
         }
 
-    if (*x < 0)
+    if ((*ball).x < 0 + (*ball).r)
         {
-        *vx = - *vx;
-        *x = 0;
+        (*ball).vx = -(*ball).vx;
+        (*ball).x  = 0 + (*ball).r;
         }
 
-    if (*y < 0)
+    if ((*ball).y < 0 + (*ball).r)
         {
-        *vy = - *vy;
-        *y = 0;
+        (*ball).vy = -(*ball).vy;
+        (*ball).y  = 0 + (*ball).r;
         }
     }
+
+//-------------------------------------------------------------
+
+void DrawScore (int score1, int score2)
+    {
+    txBegin ();
+    txSelectFont ("TimesNewRoman", 60);
+    char str[10] = "";
+    sprintf (str, "%d : %d", score1, score2);
+
+    int centerX = txGetExtentX()/2;
+
+    int textSizeX = txGetTextExtentX ("------"),
+        textSizeY = txGetTextExtentY ("------");
+
+    txSetFillColor (TX_BLACK);
+    txRectangle (centerX - textSizeX, 0,
+                 centerX + textSizeX, textSizeY);
+    txTextOut   (centerX - textSizeX/2, 0, str);
+    txEnd ();
+    }
+
+
+//-------------------------------------------------------------
+
+bool LeftGoal (struct Ball ball)
+    {
+    return (ball.y > 430) && (ball.y < 560) && (ball.x - ball.r <= 0);
+    }
+
+//-------------------------------------------------------------
+
+bool RightGoal (struct Ball ball)
+    {
+    return (ball.y > 430) && (ball.y < 560) && (ball.x + ball.r >= 1500);
+    }
+
